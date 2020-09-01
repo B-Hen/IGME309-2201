c4cd1920257e0dd2539ad5227af340158cf2dc4f
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2020-09-01 12:46:24 -0400

    Fixed issues

---
+++ Exercises/EO1/Main.cpp
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------------------------------------------------
+Code from http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+modified by labigm@rit.edu
+--------------------------------------------------------------------------------------------------*/
+#include <iostream>
+
+// Include standard headers
+#include "Main.h""
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+	getchar();
+}
