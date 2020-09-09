81eb08a82e8d6adffd6b4102eb9837d09d3343b2
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2020-09-08 22:17:45 -0400

    Finished E04

--- Exercises/include/Simplex/Shaders/BasicColor.fs
+++ Exercises/include/Simplex/Shaders/BasicColor.fs
@@ -3,7 +3,6 @@
 in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-uniform bool ComplementaryColor = false;
 
 out vec4 Fragment;
 
@@ -13,9 +12,6 @@ void main()
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
-		
-	if(ComplementaryColor == true)
-		Fragment = vec4(1 - Color.r, 1 - Color.g, 1- Color.b, 1);
 
 	return;
 }
